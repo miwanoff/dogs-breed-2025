5f5542c0fd5433cce2425e05705850685d9ff363
miwanoff <marina.iva.noffa@gmail.com>
2025-02-23 15:26:01 +0200

    Add go

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-var wrap = document.getElementById('wrap'); // Поле
-var showCoords = document.getElementById('showCoords');
+const tan = document.getElementById("tan"); // Поле
+const pink = document.getElementById("pink"); // Поле
 
-var images = document.getElementsByTagName("img"); // Ирисы
+const showCoords = document.getElementById("showCoords");
+const state = document.getElementById("state");
+
+const irises = document.getElementsByClassName("iris"); // Ириси
+
+for (let i = 0; i < irises.length; i++) {
+  // подія натискання миші
+  irises[i].addEventListener("mousedown", go);
+}
+
+function go(event) {
+    const flower = document.getElementById(event.target.id);
+    const breed = flower.dataset.breed;
+    console.log(breed);
+}
